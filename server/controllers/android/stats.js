ef7d301c47d5f9e68d2d84eeafbac68f38ded012
promanenko <paihitetty444@gmail.com>
2025-05-12 11:20:56 +0200

    feat: la correction d'un bug dans la route stats d'Android

--- server/controllers/android/stats.js
+++ server/controllers/android/stats.js
@@ -13,14 +13,13 @@ export const stats = async (req, res) => {
     }
 
     const [transactions] = await mySqlPool.query('SELECT t.date, t.montant AS price, o.type, s.nom, c.montant AS solde, s.nom AS stand FROM transactions t LEFT JOIN cartes c ON t.carte_id = c.id LEFT JOIN operations o ON t.operation_id = o.id LEFT JOIN stands s ON t.stand_id = s.id WHERE c.nfc = ?', [tagNFC]);
-    console.log(transactions)
-    const solde = transactions[0].solde
-    console.log(transactions)
 
     if (transactions.length === 0) {
         res.statusMessage = "Aucune transaction trouvée";
         return res.status(404).end();
     }
+
+    const solde = transactions[0].solde
 
     res.statusMessage = "Les statistiques ont été recupérés";
     res.status(200).json({allTransactions: transactions, solde: solde});
